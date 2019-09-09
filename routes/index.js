00db28bb7e2642b299e7c8bef0634452a3503a97
sophieannebourgine <sophieannebourgine@gmail.com>
2019-09-09 12:21:41 +0200

    signup

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.get("/signin", (req, res) => {
+  res.render("signin");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
 module.exports = router;
