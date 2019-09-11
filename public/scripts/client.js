e5b2a4bf10f272acafd2c1ccf629487ca37f2c98
sophieannebourgine <sophieannebourgine@gmail.com>
2019-09-11 17:38:25 +0200

    responsiveeeee

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,9 @@
 const burger = document.querySelector(".icon.mobile");
-const navMobile = document.getElementById("shop_nav_mobile");
-console.log(burger);
+const navMobile = document.getElementById("sidebar_main");
+console.log(navMobile);
 
 function toggleMobileNav(evt) {
+  console.log("click");
   navMobile.classList.toggle("is-active");
 }
 
