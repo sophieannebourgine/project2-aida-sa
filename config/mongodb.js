20ea2b3273b87a3f637c8ba051fadbcf4a7a632e
sophieannebourgine <sophieannebourgine@gmail.com>
2019-09-12 14:27:24 +0200

    go heroku

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,13 +1,12 @@
-
-
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useCreateIndex: true
 });
 
-mongoose.connection.on("connected", () => console.log("yay mongodb connected :)"));
+mongoose.connection.on("connected", () =>
+  console.log("yay mongodb connected :)")
+);
 
 mongoose.connection.on("error", () => console.log("nay db error sorry :("));
-
